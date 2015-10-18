a5c30ca668534a77f3df9edf8d5843cd4b001a00
alireza hor <alirezafour@gmail.com>
2015-10-18 18:05:14 +0330

    area and enviromet of circle added

---
+++ cpp/Practice/Seasen 1/Section 1/MojsazanPractice/7-area and enviroment of circle.cpp
@@ -0,0 +1,18 @@
+/*  name is alireza hor
+email address is alirezafour@gmail.com
+/*this code is print area and environment of circle
+just get the radius after that calcute area and enviroment and print it
+*/
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int radius;
+	float nPi=3.14;
+	printf("Insert Radius : ");
+	std::cin>>radius;
+	printf("Area = %2.2f \n", (radius * radius * nPi) );
+	printf("environment = %2.2f \n", (2 * radius * nPi) );
+	return 0;
+}
