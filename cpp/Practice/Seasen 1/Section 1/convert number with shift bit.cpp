0440900d1d55bff7f50d6ac3cf4caabd02fae5a4
alireza hor <alirezafour@gmail.com>
2015-10-21 11:24:47 +0330

    add comment to convert number with shift bit

--- cpp/Practice/Seasen 1/Section 1/convert number with shift bit.cpp
+++ cpp/Practice/Seasen 1/Section 1/convert number with shift bit.cpp
@@ -1,4 +1,11 @@
+/*name is alireza hor
+email address is alirezafour@gmail.com
+*/
 
+/*this code print numbers in base 8 and 16
+just use printf option
+get a number from user after that print woth %o (for base 8) and %x (for base 16)
+*/
 #include <stdio.h>
 #include <math.h>
 
@@ -9,7 +16,7 @@ int main(int argc, char const *argv[])
 	printf("Insert Number : ");
 	scanf("%d", &num);
 
-	printf("In base 8 = %o\n", num);
-	printf("In base 16 = %x\n", num);
+	printf("In base 8 = %o\n", num); //print in base 8
+	printf("In base 16 = %x\n", num); // print in base 16
 	return 0;
 }
