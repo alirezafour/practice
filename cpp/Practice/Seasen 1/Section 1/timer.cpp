0bc2716ed9e6703b5cf4ed517bee9d030d182e0e
alireza hor <alirezafour@gmail.com>
2015-10-22 18:01:32 +0330

    add comment and add color change after every minutes

--- cpp/Practice/Seasen 1/Section 1/timer.cpp
+++ cpp/Practice/Seasen 1/Section 1/timer.cpp
@@ -1,11 +1,21 @@
+/* write by alireza hor
+ email address alirezafour@gmail.com
+*/
+/* this code just show color timer
+used asci esc code to delete and change color in terminal
+this code set 3 argomane for hour , minutes and secend and one argomant for change color
+one while loop for count and print timer
+check the argomant for calcute secend and minute and hour after that print it and change color after evrey minutes
+*/
+
 #include <stdio.h>
 #include <iostream>
 #include <unistd.h>
 
 int main(int argc, char const *argv[]) {
   int secend(0), minutes(0), hour(0);
+  unsigned short int i(31);
   printf("timer : \n");
-
   while (true) {
   secend++;
   if (secend == 60) {
@@ -23,13 +33,16 @@ int main(int argc, char const *argv[]) {
       }
     }
     else {
-      printf("\033[A\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[K%02d:%02d:%02d\n", hour, minutes, secend );
+      printf("\033[A\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[K\x1b[%im%02d:%02d:%02d\n\033[0m", i++, hour, minutes, secend ); //change envioment i for change color 
     }
   }
   else {
-    printf("\033[A\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[K%02d:%02d:%02d\n", hour, minutes, secend );
+    printf("\033[A\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[C\033[K\x1b[%im%02d:%02d:%02d\n\033[0m", i, hour, minutes, secend );
   }
-  usleep(1000000);
+
+  if(i == 38)
+    i = 31;
+  usleep(1000000); //delay 1 sec
   }
   return 0;
 }
