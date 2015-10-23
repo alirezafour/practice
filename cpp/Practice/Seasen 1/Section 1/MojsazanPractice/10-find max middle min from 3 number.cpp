2c0550e11d0d8bf339ab4d7f47155fbb1bed2bd2
alireza hor <alirezafour@gmail.com>
2015-10-23 22:58:57 +0330

    fix some style and change header, replace cin with scanf

--- cpp/Practice/Seasen 1/Section 1/MojsazanPractice/10-find max middle min from 3 number.cpp
+++ cpp/Practice/Seasen 1/Section 1/MojsazanPractice/10-find max middle min from 3 number.cpp
@@ -6,37 +6,37 @@ first get 3 number from user after that compare 2 of them and set to max and min
 after it you need to compare max and min with number 3 when find where is the num3 print the result
 */
 
-#include <iostream>
-#include <stdio.h>
+
+#include <cstdio>
 
 int main(int argc, char const *argv[])
 {
 	int num1(0), num2(0), num3(0), max(0), min(0);
 	printf("Insert first number : ");
-	std::cin >> num1; // get number 1
+	scanf("%d", &num1); // get number 1
 	printf("Insert secend number : ");
-	std::cin >> num2; // get number 2
+	scanf("%d", &num2); // get number 2
 	printf("Insert thrid number : ");
-	std::cin >> num3; // get number 3
+	scanf("%d", &num3); // get number 3
 
 	// compare first 2 number and set to max and min
-	if(num1>num2)
+	if(num1 > num2)
 	{
-		max=num1;
-		min=num2;
+		max = num1;
+		min = num2;
 	}
 	else
 	{
-		max=num2;
-		min=num1;
+		max = num2;
+		min = num1;
 	}
 
-	// compare max amd min with num3 and print the result
-	if(num3>max)
+	// compare max and min with num3 and print the result
+	if(num3 > max)
 	{
 		printf("max = %d \nmiddle = %d \nmin = %d \n", num3, max, min );
 	}
-	else if (num3>min)
+	else if (num3 > min)
 	{
 		printf("max = %d \nmiddle = %d \nmin = %d \n", max, num3, min );
 	}
