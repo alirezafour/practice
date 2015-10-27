f38080dc4dfd1182e59a372413da2f2711bb9baf
alireza hor <alirezafour@gmail.com>
2015-10-27 18:01:41 +0330

    change some style

--- cpp/Practice/Seasen 1/Section 1/MojsazanPractice/39-GDA of 10 student.cpp
+++ cpp/Practice/Seasen 1/Section 1/MojsazanPractice/39-GDA of 10 student.cpp
@@ -13,18 +13,18 @@ int main(int argc, char const *argv[])
 	student std[10];
 	for (int i = 0; i < 10; ++i)
 	{
-		printf("Insert math student N%d/10: ", i);
+		printf("Insert math student N%d/10: ", (i + 1 ));
 		scanf("%f", &std[i].math);
-		printf("Insert physics student N%d/10: ", i);
+		printf("Insert physics student N%d/10: ", (i + 1));
 		scanf("%f", &std[i].physics);
-		printf("Insert chemistry student N%d/10: ", i);
+		printf("Insert chemistry student N%d/10: ", (i + 1));
 		scanf("%f", &std[i].chemistry);
 	}
 	printf("\n");
 	for (int i = 0; i < 10; ++i)
 	{
 		gda = (std[i].math + std[i].physics + std[i].chemistry) / 3;
-		printf("GDA of Strudent NO,%d is %2.2f\n", i, gda);
+		printf("GDA of Strudent NO,%d is %2.2f\n", (i + 1), gda);
 		allGda += gda;
 	}
 	printf("\nAll class GDA is %2.2f\n", (allGda / 10));
